fe99251c31b3c3fc4dc28ecc57611aa4df057697
IlikeChooros <pawel.smolnicki@gmail.com>
2025-09-08 12:03:01 +0200

    update config

--- next.config.ts
+++ next.config.ts
@@ -2,12 +2,10 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
 	/* config options here */
-	// crossOrigin: 'use-credentials',
-	// allowedDevOrigins: ['http://localhost:8080', 'http://192.168.0.103:8080'],
-	// output: 'export',
-	// images: {
-	// unoptimized: true,
-	// },
+	output: 'export',
+	images: {
+		unoptimized: true,
+	},
 };
 
 export default nextConfig;
